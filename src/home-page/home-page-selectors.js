53d20a720004a400719cce9e29ae81372b82f9e0
Martin Bryant <mail@martinbryant.me>
2017-04-26 11:34:10 +0100

    Added selectors for home page

--- src/home-page/home-page-selectors.js
+++ src/home-page/home-page-selectors.js
@@ -1,18 +1,18 @@
 import { orderBy } from 'lodash';
-import { curry, compose } from 'ramda';
+import { curry, compose, prop } from 'ramda';
 
-const getProperty = curry((prop, obj) => obj[prop]);
+// const getProperty = curry((prop, obj) => obj[prop]);
 
 const findLeague = (leagues, leagueToFind) => leagues.find(league => league._id === leagueToFind);
 
 const getFixturesOrResults = curry((status, fixtures) =>
     fixtures.filter(fixture => (status == 'fixture') ? !fixture.played : fixture.played));
 
-const findAllFixturesForSelectedLeague = compose(getProperty('fixtures'), findLeague);
+const findAllFixturesForSelectedLeague = compose(prop('fixtures'), findLeague);
 
 //exports
 
-export const findTeamsForSelectedLeague = compose(getProperty('teams'), findLeague);
+export const findTeamsForSelectedLeague = compose(prop('teams'), findLeague);
 
 export const findFixturesForSelectedLeague = compose(getFixturesOrResults('fixture'), findAllFixturesForSelectedLeague);
 
