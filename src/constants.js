9e2a04295b67b9624238f5e8b64a828bcfd2ff13
Martin Bryant <mail@martinbryant.me>
2017-04-26 11:29:35 +0100

    Added constants for all actions

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,13 @@
 export const sortArrowUp = 'glyphicon glyphicon-triangle-top';
 export const sortArrowDown = 'glyphicon glyphicon-triangle-bottom';
+
+export const LOAD_ALL_LEAGUES_SUCCESS = 'LOAD_ALL_LEAGUES_SUCCESS';
+export const CHANGE_SELECTED_LEAGUE = 'CHANGE_SELECTED_LEAGUE';
+export const SORT_TABLE_BY_COLUMN = 'SORT_TABLE_BY_COLUMN';
+export const TOGGLE_SORT_ORDER = 'TOGGLE_SORT_ORDER';
+export const EDIT_LEAGUE_NAME_SUCCESS = 'EDIT_LEAGUE_NAME_SUCCESS';
+export const ADD_LEAGUE_SUCCESS = 'ADD_LEAGUE_SUCCESS';
+export const DELETE_LEAGUE_SUCCESS = 'DELETE_LEAGUE_SUCCESS';
+export const ADD_TEAM_SUCCESS = 'ADD_TEAM_SUCCESS';
+export const EDIT_TEAM_NAME_SUCCESS = 'EDIT_TEAM_NAME_SUCCESS';
+export const DELETE_TEAM_SUCCESS = 'DELETE_TEAM_SUCCESS';
