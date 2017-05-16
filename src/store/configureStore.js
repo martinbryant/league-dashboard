31d5f494362cf7a14af7e7aee7e4a935fa0cb1c5
Martin Bryant <mail@martinbryant.me>
2017-05-16 19:58:09 +0100

    Add Redux dev tools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 
 import app from '../reducers/app-reducer';
 
-export default function configureStore(initialState){
-return createStore(app, initialState, applyMiddleware(thunk, reduxImmutableStateInvariant()));
+export default function configureStore(initialState) {
+    return createStore(app, initialState, composeWithDevTools(applyMiddleware(thunk, reduxImmutableStateInvariant())));
 }
